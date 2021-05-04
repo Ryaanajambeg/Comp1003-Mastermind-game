ba6c9ee1e07f63bc1dfe204c80b27d917e66858b
Ryaanajambeg <ryaan.ajambeg@students.plymouth.ac.uk>
2021-05-04 17:40:46 +0400

    reading user's input in the console

--- Program.cs
+++ Program.cs
@@ -4,9 +4,38 @@ namespace Comp1003_Mastermind_game
 {
     class Program
     {
+        
+        private static bool Active; //boolean variable to see if the mastermind is active or not
+
+
+
+
+
+
+
+
+
+
+        private string PlayerInput; // what the player inputs in the console
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+
+
+        private string FirstInput()
+        {
+
+            PlayerInput = ""; // input is nothing at start
+            PlayerInput = Console.ReadLine(); // read what the player inputs from the console
+            Console.Clear(); // console clears after each input
+
+            return PlayerInput;
+
         }
     }
 }

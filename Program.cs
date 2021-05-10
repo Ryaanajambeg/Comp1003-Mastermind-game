b6b751eea0c5468010b869103d738e3aa9ea276f
Ryaanajambeg <ryaan.ajambeg@students.plymouth.ac.uk>
2021-05-10 21:50:51 +0400

    randomise code working

--- Program.cs
+++ Program.cs
@@ -10,9 +10,12 @@ namespace Comp1003_Mastermind_game
         private string PlayerInput; // what the player inputs in the console
         private static bool GamePlayable; // Boolean that says game is playable if valid inputs are used
         private static bool playgame;
+        private static bool randomise = false;
 
         private static int N; // where the user selects the number of positions
         private static int M; //where the user selects the amount of colors he wants
+        private static int[] stored;
+
         static void Main(string[] args)
         {
             MastermindGame game = new MastermindGame();
@@ -26,11 +29,8 @@ namespace Comp1003_Mastermind_game
 
                 input = game.FirstInput();
                 game.ProcessInput(input);
-                
-                
+                game.Gameplay();
             }
-
-               
         }
 
 
@@ -49,7 +49,7 @@ namespace Comp1003_Mastermind_game
         {
 
             PlayerInput = userinput.ToLower();
-           
+
             if (PlayerInput == "stop")
             {
                 Active = false; // putting the boolean variable "Active" to false so as to stop the game
@@ -63,47 +63,82 @@ namespace Comp1003_Mastermind_game
             {
                 playgame = true;
             }
-                    
 
-            
+        }
 
-             if (playgame = true)
+        private void Gameplay()
+        {
+
+            if (playgame == true)
             {
-                Console.WriteLine(" Select the number of positions you wish to use");
-                if (N <= 0) // checking if the number of positions is valid
+                while (Nvalid == false && Mvalid == false)
                 {
-                    Console.WriteLine("Incorrect Input! please enter a valid number greater than 0"); // printing out an error messsage if N is invalid
+                    Console.WriteLine(" Select the number of positions you wish to use");
+                    N = Convert.ToInt32(Console.ReadLine());
+                    if (N <= 0) // checking if the number of positions is valid
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("Incorrect Input! please enter a valid number greater than 0"); // printing out an error messsage if N is invalid
+                    }
+
+                    else if (N > 0) // checking if N is valid
+                    {
+                        Console.Clear();
+                        Nvalid = true;
+                    }
                 }
 
-                if (N > 0) // checking if N is valid
+                while (Mvalid == false && Nvalid == true)
                 {
                     Console.WriteLine("Enter the number of colours you wish to use (Maximum = 9"); // printing out number of colours user wishes to enter is number of positions is valid
-                    Nvalid = true;
-                }
-                if (M! >= 1 && M! <= 9) // checking to see if M is not valid
-                {
-                    Console.WriteLine("Invalid number of colours, please enter a number between 1 and 9"); // printing out an error message if the range of M is not valid
-                }
-                if (M >=1 && M<=9)
-                {
-                    Mvalid = true;
+                    M = Convert.ToInt32(Console.ReadLine());
+                    if (M < 1 || M > 9) // checking to see if M is not valid
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Invalid number of colours, please enter a number between 1 and 9"); // printing out an error message if the range of M is not valid
+                    }
+                    else if (M >= 1 || M <= 9)
+                    {
+                        Console.Clear();
+                        Mvalid = true;
+                    }
+
+
                 }
             }
 
+            if (Mvalid = true && Nvalid == true && randomise == false)
+            {
+
+
+                stored = new int[N];
+
+                Random rand = new Random();
+
+                for (int i = 0; i < N; i++)
+                {
+                    int temporary = rand.Next(1, M + 1);
+                    stored[i] = temporary;
+                }
+                for (int i = 0; i < N; i++)
+                {
+                    Console.Write(stored[i]);
+                }
+                Console.WriteLine();
+
+                randomise = true;
+
+            }
+
+
+
         }
 
-        private void Gameplay (bool inplay)
+        private void PositionsandColours(string array)
         {
-         
+
 
         }
-
-        private void PositionsandColours (string array)
-        {
-            
-
-        }
-
-
     }
 }
